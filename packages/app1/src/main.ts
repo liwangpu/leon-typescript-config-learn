a9fb5976ee11b17665aab54a9bf2fe2961ed3b41
蒲立望60263 <liwang.pu@going-link.com>
2023-01-06 14:02:58 +0800

    1

---
+++ packages/app1/src/main.ts
@@ -0,0 +1,3 @@
+import { driveCar } from 'cart';
+
+driveCar();
---
+++ packages/cart/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = (env) => {
+  return {
+    mode: 'development',
+    devtool: false,
+    entry: {
+      index: './src/index.ts',
+    },
+    output: {
+      path: path.resolve(__dirname, '../../dist/cart'),
+      clean: true,
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+  };
+};
